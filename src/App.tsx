e3a6912442790dd81331f9f9374d98c6d5571ca3
Carlos Beclhior <cbelchior@coamo.com.br>
2025-11-28 16:51:06 -0300

    código não utilizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import './App.css'
-import { DeviceSelectionExample } from './ScanBarCode'
 
+import { DeviceSelectionExample } from './ScanBarCode
 function App() {
 
   return (

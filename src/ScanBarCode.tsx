8859acd5ce27b8c8c11897eef0837ccaf8e30fd7
Carlos Beclhior <cbelchior@coamo.com.br>
2025-12-01 10:33:07 -0300

    Corrigindo leitura

--- src/ScanBarCode.tsx
+++ src/ScanBarCode.tsx
@@ -4,33 +4,56 @@ import BarcodeScannerComponent from "react-qr-barcode-scanner";
 export const DeviceSelectionExample = () => {
   const [data, setData] = useState("");
   const [isMobile, setIsMobile] = useState(false);
+  const [isPortrait, setIsPortrait] = useState(true);
 
-  useEffect(() => {
+
+   useEffect(() => {
     const checkMobile = /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
     setIsMobile(checkMobile);
+
+    function handleOrientation() {
+      const portrait = window.innerHeight > window.innerWidth;
+      setIsPortrait(portrait);
+    }
+
+    handleOrientation(); // executa ao abrir
+
+    window.addEventListener("resize", handleOrientation);
+    window.addEventListener("orientationchange", handleOrientation);
+
+    return () => {
+      window.removeEventListener("resize", handleOrientation);
+      window.removeEventListener("orientationchange", handleOrientation);
+    };
   }, []);
 
-  return (
+   return (
     <div style={{ padding: 20, textAlign: "center" }}>
       <h2>Leitor EAN-13</h2>
 
-      <div className={isMobile ? "scanner-wrapper mobile" : "scanner-wrapper"}>
+      <div
+        className={
+          isMobile && isPortrait
+            ? "scanner-wrapper mobile portrait"
+            : isMobile && !isPortrait
+            ? "scanner-wrapper mobile landscape"
+            : "scanner-wrapper"
+        }
+      >
         <BarcodeScannerComponent
           width="100%"
           height="100%"
           onUpdate={(err, result: any) => {
             console.log('err', err)
-            if (result) {
-              setData(result.text);
-            }
+            if (result) setData(result.text);
           }}
         />
       </div>
 
       <h3 style={{ marginTop: 20 }}>
-        Resultado:
+        Resultado:{" "}
         <strong style={{ color: "#572580" }}>
-          {data || " Aguardando leitura..."}
+          {data || "Aguardando leitura..."}
         </strong>
       </h3>
     </div>

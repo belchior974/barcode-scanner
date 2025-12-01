8193fef1a7dbfe22d5391c73690b2cf0e6449c6a
Carlos Beclhior <cbelchior@coamo.com.br>
2025-12-01 10:26:47 -0300

    Ajustando leitor

--- src/ScanBarCode.tsx
+++ src/ScanBarCode.tsx
@@ -1,27 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BarcodeScannerComponent from "react-qr-barcode-scanner";
 
 export const DeviceSelectionExample = () => {
   const [data, setData] = useState("");
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+    setIsMobile(checkMobile);
+  }, []);
 
   return (
     <div style={{ padding: 20, textAlign: "center" }}>
-      <h2>Leitor de Código de Barras (EAN-13)</h2>
+      <h2>Leitor EAN-13</h2>
 
-      <div
-        style={{
-          width: "100%",
-          maxWidth: 700,
-          height: 250,
-          margin: "0 auto",
-          border: "3px solid #00FF9D",
-          borderRadius: "8px",
-          overflow: "hidden",
-        }}
-      >
+      <div className={isMobile ? "scanner-wrapper mobile" : "scanner-wrapper"}>
         <BarcodeScannerComponent
-          width={"100%"}
-          height={"100%"}
+          width="100%"
+          height="100%"
           onUpdate={(err, result: any) => {
             console.log('err', err)
             if (result) {
@@ -32,10 +28,10 @@ export const DeviceSelectionExample = () => {
       </div>
 
       <h3 style={{ marginTop: 20 }}>
-        Resultado:{" "}
-        <span style={{ color: "#572580", fontWeight: "bold" }}>
-          {data || "Aguardando leitura..."}
-        </span>
+        Resultado:
+        <strong style={{ color: "#572580" }}>
+          {data || " Aguardando leitura..."}
+        </strong>
       </h3>
     </div>
   );

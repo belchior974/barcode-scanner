b5b844c45fdc4a914aabade14cc19486935e4dd7
Carlos Beclhior <cbelchior@coamo.com.br>
2025-12-01 16:52:11 -0300

    trocando para o react-qr-code-scanner

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
 // import { DeviceSelectionExample } from "./ScanBarCode";
-import { BarcodeScanner } from "./QuaggaBarReader";
+// import { BarcodeScanner } from "./QuaggaBarReader";
+import { DeviceSelectionExample } from "./ScanBarCode";
 
 function App() {
-  const [scannedCode, setScannedCode] = useState(null);
+  // const [scannedCode, setScannedCode] = useState(null);
 
-  const handleBarcodeDetected = (result: any) => {
-    if (result && result.codeResult) {
-      setScannedCode(result.codeResult.code);
-      // Optionally, stop scanning after a successful scan
-      // Quagga.stop();
-    }
-  };
+  // const handleBarcodeDetected = (result: any) => {
+  //   if (result && result.codeResult) {
+  //     setScannedCode(result.codeResult.code);
+  //     // Optionally, stop scanning after a successful scan
+  //     // Quagga.stop();
+  //   }
+  // };
 
   return (
     <div>
       <h1>React Barcode Scanner</h1>
-      {scannedCode ? (
+      {/* {scannedCode ? (
         <p>Scanned Code: {scannedCode}</p>
-      ) : (
-        <BarcodeScanner onDetected={handleBarcodeDetected} />
-      )}
+      ) : ( */}
+        <DeviceSelectionExample  />
+      {/* // )} */}
     </div>
   );
 }
